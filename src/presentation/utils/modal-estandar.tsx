8948e1e705c008aca884954b895f303206ca8d8f
Break <franklinbacacampos@gmail.com>
2026-07-26 16:19:08 -0500

    add borde en el header del modal tras del fix de tailwind vs mantine

--- src/presentation/utils/modal-estandar.tsx
+++ src/presentation/utils/modal-estandar.tsx
@@ -39,7 +39,9 @@ export const ModalEstandar = ({
             </span>
           </div>
           {rightSection && (
-            <div className="flex items-center flex-none mr-4">{rightSection}</div>
+            <div className="flex items-center flex-none mr-4">
+              {rightSection}
+            </div>
           )}
         </div>
       }
@@ -50,9 +52,10 @@ export const ModalEstandar = ({
       }}
       radius="xl"
       classNames={{
-        content: "bg-zinc-950 border border-white/10 shadow-2xl shadow-black [scrollbar-width:none] [&::-webkit-scrollbar]:hidden",
-        header: "bg-zinc-950 text-white pt-5 pb-1 px-6",
-        body: "bg-zinc-950 px-6 pt-6 pb-6 [scrollbar-width:none] [&::-webkit-scrollbar]:hidden",
+        content: "bg-zinc-950 border border-white/10 shadow-2xl shadow-black",
+        header:
+          "bg-zinc-950 text-white pt-5 pb-4 px-6 border-b border-white/10",
+        body: "bg-zinc-950 px-6 pt-6 pb-6",
         close: `text-zinc-400 hover:text-white hover:bg-white/10 transition-all 
           duration-200 rounded-full w-8 h-8 flex items-center justify-center`,
         title: "text-xl font-bold text-white flex-1",
